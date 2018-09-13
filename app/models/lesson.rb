bae3330d307b7f63bb50f981b6e235ad8d3c91c2
thotranuet97 <thotranuet97@gmail.com>
2018-09-13 15:39:12 +0700

    update header

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,6 +3,7 @@ class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :grade
   belongs_to :subject
+  validates :title_content, presence: true
   default_scope -> { order(created_at: :desc) }
   scope :grade_id, -> (grade_id) { where grade_id: grade_id }
   scope :subject_id, -> (subject_id) { where subject_id: subject_id }
